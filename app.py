ed512a9bd89664e8b548ce249cb0991b65e0ecb0
deytalytics <james_dey@hotmail.com>
2023-11-24 21:43:42 +0000

    Bootstrap5 and templating the web page

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from authlib.integrations.flask_client import OAuth
-from flask_login import LoginManager, login_user, current_user, login_required
 import os
 
 oauth = OAuth()
-login_manager = LoginManager()
 
 def create_app():
 
@@ -30,13 +28,6 @@ def create_app():
 
 app = create_app()
 app.secret_key=os.getenv('SECRET_KEY')
-
-# User loader callback
-@login_manager.user_loader
-def load_user(user_id):
-    # Here, you should write the logic to load a user from your database using the user_id
-    # and return the user object. For now, let's assume that the user object is stored in session.
-    return session.get('user')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
