1f7f7ddbb612c5674e7f41f11be190bbcdf3f36d
deytalytics <james_dey@hotmail.com>
2023-11-30 18:48:39 +0000

    Each user now has to login to save and delete stocks. When they login their last saved stocks are pulled from database.

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, request, session, render_template, redirect, url_for
+from sqlalchemy import create_engine, text
 from authlib.integrations.flask_client import OAuth
 import os, uuid, json
 import plotly.graph_objs as go
 import plotly.offline as pyo
 import pandas as pd
-
 from stocks import stocks, ftse_100_stocks
-
-from sqlalchemy import create_engine, delete
 
 def create_app():
 
@@ -18,7 +16,7 @@ def create_app():
     # Initialize oauth with the app
     oauth.init_app(app)
 
-    google = oauth.register(
+    oauth.register(
         name='google',
         server_metadata_url=CONF_URL,
         client_kwargs={
@@ -26,20 +24,20 @@ def create_app():
         }
     )
 
+    # Initialize the global engine variable
+    global engine
+    engine = connect_db()
+
     return app
 
-
-from sqlalchemy import create_engine, text
-
-from sqlalchemy import create_engine, text
-
-
-def delete_portfolio(email):
+def connect_db():
     username = os.getenv('USER')
     password = os.getenv('PASSWORD')
     engine = create_engine(
         f'postgresql://{username}:{password}@postgres-srvr.postgres.database.azure.com/data_product_metadata')
+    return engine
 
+def delete_user_stocks(email):
     # Establish a connection
     with engine.connect() as connection:
         # Start a new transaction
@@ -48,26 +46,41 @@ def delete_portfolio(email):
             delete_query = text(f"DELETE FROM user_stocks WHERE email = '{email}'")
             connection.execute(delete_query)
 
-def save_portfolio(email):
-    username = os.getenv('USER')
-    password = os.getenv('PASSWORD')
-    engine = create_engine(
-        f'postgresql://{username}:{password}@postgres-srvr.postgres.database.azure.com/data_product_metadata')
-
-    delete_portfolio(email)
-
+def save_user_stocks(email):
+    delete_user_stocks(email)
     # Create and populate a dataframe
-    df = pd.DataFrame({'email': [email] * len(stocks), 'stocks': stocks})
-    # Store the data in PostgreSQL
+    df = pd.DataFrame(user_stocks)
+    # Store the user_stocks in the database
     df.to_sql('user_stocks', engine, if_exists='append', index=False)
+
+def load_all_user_stocks():
+    #fetch user's stocks
+    query = "select * from user_stocks"
+
+    # Create a connection and execute the query
+    with engine.connect() as connection:
+        result = connection.execute(text(query))
+
+    # Fetch the resultset
+    user_stocks_list = result.fetchall()
+
+    return user_stocks_list
+
+def load_stock_price_history():
+    #fetch the stock_price_history from the database
+    query = "select * from stock_price_history"
+
+    # Use pandas to execute the query and load the data into a DataFrame
+    stock_price_history = pd.read_sql_query(query, engine)
+
+    # Make sure 'reported_date' is a datetime object
+    stock_price_history['reported_date'] = pd.to_datetime(stock_price_history['reported_date'])
+
+    return stock_price_history
 
 
 #Load the 1 year, 2 year, 5 year and 10 year percentage increases for a specific stock
-def load_stock_data(stock):
-    # Create a connection to your PostgreSQL database
-    username = os.getenv('USER')
-    password = os.getenv('PASSWORD')
-    engine = create_engine(f'postgresql://{username}:{password}@postgres-srvr.postgres.database.azure.com/data_product_metadata')
+def load_stock_returns_old(stock):
     stock_returns = []
     for period, days in time_periods.items():
         # Execute the SQL query and fetch the data
@@ -87,7 +100,29 @@ LIMIT 2;"""
             stock_returns.append(percentage_return)
         else:
             stock_returns.append(None)
-    engine.dispose()
+    return stock_returns
+
+
+def load_stock_returns(stock, stock_price_history):
+    stock_returns = []
+    for period, days in time_periods.items():
+        # Filter the data for the specific stock
+        stock_data = stock_price_history[stock_price_history['stock_symbol'] == stock]
+
+        # Get the latest date for the stock
+        max_date = stock_data['reported_date'].max()
+
+        # Get the data for the latest date and the date 'days' ago
+        stock_data = stock_data[(stock_data['reported_date'] == max_date) |
+                                (stock_data['reported_date'] <= max_date - pd.Timedelta(days=days))].sort_values(
+            'reported_date').tail(2)
+
+        if len(stock_data) > 0:  # Check if data is available
+            percentage_return = round(
+                (stock_data['Close'].iloc[-1] - stock_data['Close'].iloc[0]) / stock_data['Close'].iloc[0] * 100, 2)
+            stock_returns.append(percentage_return)
+        else:
+            stock_returns.append(None)
     return stock_returns
 
 #Create the plot for all of the stocks in the stock portfolio
@@ -125,18 +160,18 @@ def get_email():
         email = None
     return email
 
+engine = None
+
 oauth = OAuth()
 
 app = create_app()
 app.secret_key=os.getenv('SECRET_KEY')
 
-all_stock_data = {}
 time_periods = {"1y": 365, "2y": 365*2, "3y": 365*3, "4y":365*4, "5y": 365*5, "6y": 365*6, "7y":365*7, "8y":365*8, "9y":365*9, "10y": 365*10}
 
-returns = {}
-
-for stock in stocks:
-    returns[stock] = load_stock_data(stock)
+returns={}
+user_stocks = load_all_user_stocks()
+stock_price_history = load_stock_price_history()
 
 @app.route('/')
 def homepage():
@@ -156,38 +191,40 @@ def legal():
 
 @app.route('/plot',methods=['GET','POST'])
 def get_stocks_returns():
+    returns={}
+    global user_stocks
     if request.method == 'POST':
+        email = get_email()
         action = request.form['action']
         if action == 'add':
-            #Add the selected stock to the portfolio
             selected_stock = request.form['stock']
-            # Add the selected stock to the portfolio
-            if selected_stock in ftse_100_stocks.values() and selected_stock not in stocks:
-                stocks.append(selected_stock)
-                returns[selected_stock] = load_stock_data(selected_stock)
-            elif selected_stock not in ftse_100_stocks.values():
-                return "<script>alert('The selected stock is not in the FTSE 100 list. Please select a valid stock.')</script>"
-            else:
-                return "<script>alert('The selected stock is already in the list. Please select a different stock.')</script>"
+            # Check if the selected stock already exists for the specified email
+            if (email, selected_stock) not in user_stocks:
+                # If it doesn't exist, append it to the list and the database
+                user_stocks.append((email, selected_stock))
         elif action == 'remove':
             #Remove the selected stock from the portfolio
             selected_stock = request.form['stock']
-            if selected_stock in stocks:
-                stocks.remove(selected_stock)
-                del returns[selected_stock]
-            else:
-                return "<script>alert('The selected stock is not in the current list. Please select a different stock to remove.')</script>"
+            user_email = get_email()
+            # Check if the selected stock already exists for the specified email
+            if (email, selected_stock) in user_stocks:
+                # If it exists then remove it from user_stocks
+                user_stocks.remove((email, selected_stock))
         elif action == 'save':
             #Save the stock portfolio to the database
-            save_portfolio(get_email())
+            save_user_stocks(get_email())
         elif action == 'delete':
             #Delete all of the stocks from the portfolio belonging to the user's email
-            delete_portfolio(get_email())
+            delete_user_stocks(get_email())
+            user_stocks = load_all_user_stocks()
 
-
-    username = get_username()
+    #Construct the plotly plot
+    email = get_email()
+    for stock in user_stocks:
+        if stock[0]==email:
+            returns[stock[1]] = load_stock_returns(stock[1], stock_price_history)
     plot_div = load_plots(returns)
-    return render_template('stocks.html', user = username, ftse_100_stocks=ftse_100_stocks, plot_div=plot_div)
+    return render_template('stocks.html', user = get_username(), ftse_100_stocks=ftse_100_stocks, plot_div=plot_div)
 
 @app.route('/login')
 def login():
@@ -202,7 +239,6 @@ def logout():
     session['state'] = ''
     session['user']=''
     username = get_username()
-    print (request.path, request.referrer)
     return redirect(request.referrer)
 
 @app.route('/authorize')
@@ -213,7 +249,6 @@ def authorize():
         return f"Error: state mismatch request:{request.args.get('state','')} session:{session.get('state','')}"
     token = oauth.google.authorize_access_token()
     session['user'] = token['userinfo']
-    print(session['referrer'])
     return redirect(session.get('referrer','/'))
 
 if __name__ == '__main__':
